d0559233be3e7d59498b44fe6db69910e3657647
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2023-12-11 13:23:11 -0300

    feat: initialized division of the films part

--- src/app/components/MoviesPage.tsx
+++ src/app/components/MoviesPage.tsx
@@ -3,13 +3,27 @@ import Image from "next/image"
 
 export default function MoviesPage() {
   return (
-    <div className="flez flex-col items-start justify-center h-screen w-[100%] bg-black">
+    <div id="container"className="flex flex-row items-center justify-center h-screen w-[100%] bg-black">
        {/* <Image
           src={screenshot_Page_Movies}
           className="h-screen w-[100%] blur-[3.5px] brightness-75"
           alt="Picture of initial screen"
           quality={100}
           /> */}
+
+          <div className="bg-slate-700 flex items-center justify-center flex-col w-[50%] h-screen">
+
+            <div className="bg-green-500 rounded-lg w-[70%] h-[80%] m-2">
+              <Image src={""} alt={""}></Image>
+            </div>
+
+            <div className="bg-green-400 w-[70%] h-6"></div>
+
+          </div>
+
+          <div className=" bg-zinc-700 flex flex-col w-[50%] h-screen">
+
+          </div>
       
     </div>
   )
