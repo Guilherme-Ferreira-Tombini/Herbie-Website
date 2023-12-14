9859c79ce84a69368e2612d9d1a3c191d774b826
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2023-12-14 09:14:54 -0300

    feat: modified style of passage of films

--- src/app/components/MoviesPage.tsx
+++ src/app/components/MoviesPage.tsx
@@ -12,6 +12,12 @@ export default function MoviesPage() {
         return '/img/Herbie1.jpg';
       case 'option2':
         return '/img/Herbie2.jpg';
+      case 'option3':
+        return '/img/Herbie3.jpg';
+      case 'option4':
+        return '/img/Herbie4.jpg';
+      case 'option5':
+        return '/img/Herbie5.jpg';
       default:
         return '/img/tela.jpg'; // imagem padrão se a opção não for reconhecida
     }
@@ -29,10 +35,11 @@ export default function MoviesPage() {
           <div className="bg-slate-700 flex items-center justify-center flex-col w-[50%] h-screen">
 
             {/* Imagens dos filmes - inicial */}
-            <div className="bg-green-500 rounded-lg w-[70%] h-[80%] m-2">
+            <div className="w-[70%] h-[80%] m-2">
                <Image 
                 src={imageSrc} 
                 alt="Imagem" 
+                className=" rounded-3xl"
                 width={0} 
                 height={0} 
                 sizes="100vw" 
@@ -42,20 +49,88 @@ export default function MoviesPage() {
             {/* Imagens dos filmes - fim */}
 
             {/* Opções de filmes - inicial */}
-            <div className="bg-green-400 w-[70%] h-6">
-                <input
-                  type="radio"
-                  value="option1"
-                  checked={selectedOption === 'option1'}
-                  onChange={() => handleOptionChange('option1')}
-                />
+            <div className="w-[70%] h-6">
+              <div className=" bg-red-600 w-[30%] rounded-lg flex flex-grow justify-center items-start p-1">
+                  <li className="radio mr-1 box-border float-left h-[13px] relative w-[13px]">
+                    <label
+                      className={`cursor-pointer absolute w-full h-full mt-[-24px] flex items-center justify-center rounded-lg whitespace-nowrap ${
+                        selectedOption === 'option1' ? 'bg-red-500' : 'bg-white'
+                      }`}
+                    >
+                      <input
+                      type="radio"
+                      value="option1"
+                      checked={selectedOption === 'option1'}
+                      onChange={() => handleOptionChange('option1')}
+                      className="all:unset absolute w-full h-full top-0 left-0 cursor-pointer opacity-0"
+                    />
+                    </label>
+                  </li>
 
-                <input
-                  type="radio"
-                  value="option2"
-                  checked={selectedOption === 'option2'}
-                  onChange={() => handleOptionChange('option2')}
-                />
+                  <li className="radio mr-1 box-border float-left h-[13px] relative w-[13px]">
+                    <label
+                      className={`cursor-pointer absolute w-full h-full mt-[-24px] flex items-center justify-center rounded-lg whitespace-nowrap ${
+                        selectedOption === 'option2' ? 'bg-red-500' : 'bg-white'
+                      }`}
+                    >
+                      <input
+                      type="radio"
+                      value="option2"
+                      checked={selectedOption === 'option2'}
+                      onChange={() => handleOptionChange('option2')}
+                      className="all:unset absolute w-full h-full top-0 left-0 cursor-pointer opacity-0"
+                    />
+                    </label>
+                  </li>
+
+                  <li className="radio mr-1 box-border float-left h-[13px] relative w-[13px]">
+                    <label
+                      className={`cursor-pointer absolute w-full h-full mt-[-24px] flex items-center justify-center rounded-lg whitespace-nowrap ${
+                        selectedOption === 'option3' ? 'bg-red-500' : 'bg-white'
+                      }`}
+                    >
+                      <input
+                      type="radio"
+                      value="option3"
+                      checked={selectedOption === 'option3'}
+                      onChange={() => handleOptionChange('option3')}
+                      className="all:unset absolute w-full h-full top-0 left-0 cursor-pointer opacity-0"
+                    />
+                    </label>
+                  </li>
+
+                  <li className="radio mr-1 box-border float-left h-[13px] relative w-[13px]">
+                    <label
+                      className={`cursor-pointer absolute w-full h-full mt-[-24px] flex items-center justify-center rounded-lg whitespace-nowrap ${
+                        selectedOption === 'option4' ? 'bg-red-500' : 'bg-white'
+                      }`}
+                    >
+                      <input
+                      type="radio"
+                      value="option4"
+                      checked={selectedOption === 'option4'}
+                      onChange={() => handleOptionChange('option4')}
+                      className="all:unset absolute w-full h-full top-0 left-0 cursor-pointer opacity-0"
+                    />
+                    </label>
+                  </li>
+
+                  <li className="radio mr-1 box-border float-left h-[13px] relative w-[13px]">
+                    <label
+                      className={`cursor-pointer absolute w-full h-full mt-[-24px] flex items-center justify-center rounded-lg whitespace-nowrap ${
+                        selectedOption === 'option5' ? 'bg-red-500' : 'bg-white'
+                      }`}
+                    >
+                      <input
+                      type="radio"
+                      value="option5"
+                      checked={selectedOption === 'option5'}
+                      onChange={() => handleOptionChange('option5')}
+                      className="all:unset absolute w-full h-full top-0 left-0 cursor-pointer opacity-0"
+                    />
+                    </label>
+                  </li>
+              </div>
             </div>
             {/* Opções de filmes - fim */}
 
