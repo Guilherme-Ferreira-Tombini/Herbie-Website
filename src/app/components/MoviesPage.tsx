223f70f50e0290dc9fba79efa562bddf42ca513b
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2023-12-16 15:52:50 -0300

    feat: added other images and system to show the title of the films

--- src/app/components/MoviesPage.tsx
+++ src/app/components/MoviesPage.tsx
@@ -18,12 +18,36 @@ export default function MoviesPage() {
         return '/img/Herbie4.jpg';
       case 'option5':
         return '/img/Herbie5.jpg';
+      case 'option6':
+        return '/img/Herbie6.jpg';
       default:
         return '/img/tela.jpg'; // imagem padrão se a opção não for reconhecida
     }
   };
 
+  const getTitle = (option: string): string => {
+    // Lógica para determinar a imagem com base na opção selecionada
+    switch (option) {
+      case 'option1':
+        return "Se Meu Fusca Falasse (1968)";
+      case 'option2':
+        return "As Novas Aventuras do Fusca (1974)";
+      case 'option3':
+        return "O Fusca Enamorado (1977)";
+      case 'option4':
+        return "A Última Cruzada do Fusca (1980)";
+      case 'option5':
+        return "The Love Bug (1997)";
+      case 'option6':
+        return "Herbie - Meu Fusca Turbinado (2005)";
+      default:
+        return "Texto sobre o resumo dos filmes"; // Texto padrão se a opção não for reconhecida
+    }
+  };
+
   const imageSrc = getImageSrc(selectedOption);
+  const title = getTitle(selectedOption);
+  //const resume = getResume(selectedOption);
 
   const handleOptionChange = (newOption: string) => {
     setSelectedOption(newOption);
@@ -50,10 +74,10 @@ export default function MoviesPage() {
 
             {/* Opções de filmes - inicial */}
             <div className="w-[70%] h-6">
-              <div className=" bg-red-600 w-[30%] rounded-lg flex flex-grow justify-center items-start p-1">
-                  <li className="radio mr-1 box-border float-left h-[13px] relative w-[13px]">
+              <div className=" bg-red-600 w-[30%] rounded-lg flex flex-grow justify-center items-start h-5">
+                  <li className="radio mr-1 box-border float-left h-[13.5px] relative w-[13.5px]">
                     <label
-                      className={`cursor-pointer absolute w-full h-full mt-[-24px] flex items-center justify-center rounded-lg whitespace-nowrap ${
+                      className={`cursor-pointer absolute w-full h-full mt-[-21px] flex items-center justify-center rounded-lg whitespace-nowrap ${
                         selectedOption === 'option1' ? 'bg-red-500' : 'bg-white'
                       }`}
                     >
@@ -67,9 +91,9 @@ export default function MoviesPage() {
                     </label>
                   </li>
 
-                  <li className="radio mr-1 box-border float-left h-[13px] relative w-[13px]">
+                  <li className="radio mr-1 box-border float-left h-[13.5px] relative w-[13.5px]">
                     <label
-                      className={`cursor-pointer absolute w-full h-full mt-[-24px] flex items-center justify-center rounded-lg whitespace-nowrap ${
+                      className={`cursor-pointer absolute w-full h-full mt-[-21px] flex items-center justify-center rounded-lg whitespace-nowrap ${
                         selectedOption === 'option2' ? 'bg-red-500' : 'bg-white'
                       }`}
                     >
@@ -83,9 +107,9 @@ export default function MoviesPage() {
                     </label>
                   </li>
 
-                  <li className="radio mr-1 box-border float-left h-[13px] relative w-[13px]">
+                  <li className="radio mr-1 box-border float-left h-[13.5px] relative w-[13.5px]">
                     <label
-                      className={`cursor-pointer absolute w-full h-full mt-[-24px] flex items-center justify-center rounded-lg whitespace-nowrap ${
+                      className={`cursor-pointer absolute w-full h-full mt-[-21px] flex items-center justify-center rounded-lg whitespace-nowrap ${
                         selectedOption === 'option3' ? 'bg-red-500' : 'bg-white'
                       }`}
                     >
@@ -99,9 +123,9 @@ export default function MoviesPage() {
                     </label>
                   </li>
 
-                  <li className="radio mr-1 box-border float-left h-[13px] relative w-[13px]">
+                  <li className="radio mr-1 box-border float-left h-[13.5px] relative w-[13.5px]">
                     <label
-                      className={`cursor-pointer absolute w-full h-full mt-[-24px] flex items-center justify-center rounded-lg whitespace-nowrap ${
+                      className={`cursor-pointer absolute w-full h-full mt-[-21px] flex items-center justify-center rounded-lg whitespace-nowrap ${
                         selectedOption === 'option4' ? 'bg-red-500' : 'bg-white'
                       }`}
                     >
@@ -115,9 +139,9 @@ export default function MoviesPage() {
                     </label>
                   </li>
 
-                  <li className="radio mr-1 box-border float-left h-[13px] relative w-[13px]">
+                  <li className="radio mr-1 box-border float-left h-[13.5px] relative w-[13.5px]">
                     <label
-                      className={`cursor-pointer absolute w-full h-full mt-[-24px] flex items-center justify-center rounded-lg whitespace-nowrap ${
+                      className={`cursor-pointer absolute w-full h-full mt-[-21px] flex items-center justify-center rounded-lg whitespace-nowrap ${
                         selectedOption === 'option5' ? 'bg-red-500' : 'bg-white'
                       }`}
                     >
@@ -130,6 +154,22 @@ export default function MoviesPage() {
                     />
                     </label>
                   </li>
+
+                  <li className="radio mr-1 box-border float-left h-[13.5px] relative w-[13.5px]">
+                    <label
+                      className={`cursor-pointer absolute w-full h-full mt-[-21px] flex items-center justify-center rounded-lg whitespace-nowrap ${
+                        selectedOption === 'option6' ? 'bg-red-500' : 'bg-white'
+                      }`}
+                    >
+                      <input
+                      type="radio"
+                      value="option6"
+                      checked={selectedOption === 'option6'}
+                      onChange={() => handleOptionChange('option6')}
+                      className="all:unset absolute w-full h-full top-0 left-0 cursor-pointer opacity-0"
+                    />
+                    </label>
+                  </li>
               </div>
             </div>
             {/* Opções de filmes - fim */}
@@ -137,6 +177,9 @@ export default function MoviesPage() {
           </div>
 
           <div className=" bg-zinc-700 flex items-center justify-center flex-col w-[50%] h-screen">
+
+            <h1 className=" text-center text-3xl font-medium text-white">{title}</h1>
+            {/* <p className="">{resume}</p> */}
 
           </div>
       
