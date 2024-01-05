44be8d8264fec705258e48b31727d4edee19673a
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2024-01-04 23:27:12 -0300

    feat: added page separator

---
+++ src/app/components/SeparatorPage.tsx
@@ -0,0 +1,10 @@
+export default function SeparatorPage() {
+  return (
+   <div className="w-[100%] h-2 absolute top-screen">
+
+     <div className="bg-red-600 w-[100%] h-[1.5px]"/>
+     <div className="bg-white w-[100%] h-[2.5px]"/>
+     <div className="bg-blue-800 w-[100%] h-[4px]"/>
+   </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import InitialPage from './components/InitialPage'
 import MoviesPage from './components/MoviesPage'
+import SeparatorPage from './components/SeparatorPage'
 
 export default function Home() {
   return (
    <> 
     <InitialPage/>
+    <SeparatorPage/>
     <MoviesPage/>
   
    </>
